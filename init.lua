851908193c6c0f746ccfa7c3f3f029fa83063408
prisch <rischpierre@gmail.com>
2022-09-28 10:23:31 -0400

    bootstrap packer instead of having it in the repo

--- init.lua
+++ init.lua
@@ -48,6 +48,19 @@ vim.g.fzf_layout = {window = {down = '40%'}}
 -- nnoremap <leader>r :call vimspector#RunToCursor()<CR>
 -- nnoremap <leader>bp :call vimspector#ToggleBreakpoint()<CR>
 
+-- bootstrap packer
+local ensure_packer = function()
+  local fn = vim.fn
+  local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+  if fn.empty(fn.glob(install_path)) > 0 then
+    fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+    vim.cmd [[packadd packer.nvim]]
+    return true
+  end
+  return false
+end
+local packer_bootstrap = ensure_packer()
+
 return require('packer').startup(function(use)
 
     use 'github/copilot.vim'
