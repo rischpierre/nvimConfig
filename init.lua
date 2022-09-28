477d98ee8d701b9062eed9af29c5c0e6351b8fd6
pierre <rischpierre@gmail.com>
2022-09-27 22:14:00 -0400

    convert init in lua and add packer plugin

---
+++ init.lua
@@ -0,0 +1,61 @@
+vim.opt.number = true
+
+vim.opt.clipboard = "unnamedplus" -- in order to use: "+y to put the yank register into the clipboard
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+
+vim.opt.ignorecase = true 
+vim.opt.smartcase = true -- both need to set for the smartcase
+-- vim.opt.spell = "spelllang=en_us"
+
+vim.opt.mouse = "a" -- enable mouse in normal mode
+vim.opt.splitbelow = true
+vim.opt.background = "light"
+
+vim.g.mapleader = " "
+vim.api.nvim_set_keymap("n", "<leader>v", ":e $MYVIMRC<CR>", {noremap = true, silent = true})
+vim.api.nvim_set_keymap("n", "<A-j>", "<C-w>j", {noremap = true, silent = true})
+vim.api.nvim_set_keymap("n", "<A-k>", "<C-w>k", {noremap = true, silent = true})
+vim.api.nvim_set_keymap("n", "<A-h>", "<C-w>h", {noremap = true, silent = true})
+vim.api.nvim_set_keymap("n", "<A-l>", "<C-w>l", {noremap = true, silent = true})
+vim.api.nvim_set_keymap("n", "<A-c>", '":+y<CR>', {noremap = true, silent = true})
+vim.api.nvim_set_keymap("t", "<Esc>", "<C-\\><C-n>", {noremap = true, silent = true}) -- in order to use ESC to exit terminal mode
+vim.api.nvim_set_keymap("n", "<Esc>", ":noh<CR>", {noremap = true, silent = true})
+
+
+vim.g.auto_save = true
+
+-- Fzf config
+vim.api.nvim_set_keymap("n", "<C-p>", ":Files<CR>", {noremap = true, silent = false})
+vim.api.nvim_set_keymap("n", "<C-o>", ":Buffers<CR>", {noremap = true, silent = false})
+vim.g.fzf_layout = {window = {down = '40%'}}
+
+-- vimspector config
+-- TODO
+-- nnoremap <leader>sd :call vimspector#Launch()<CR>
+-- nnoremap <leader>kd :call vimspector#Reset()<CR>
+-- nnoremap <leader>c :call vimspector#Continue()<CR>
+-- nnoremap <leader>n :call vimspector#StepOver()<CR>
+-- nnoremap <leader>so :call vimspector#StepOut()<CR>
+-- nnoremap <leader>si :call vimspector#StepInto()<CR>
+-- nnoremap <leader>r :call vimspector#RunToCursor()<CR>
+-- nnoremap <leader>bp :call vimspector#ToggleBreakpoint()<CR>
+
+return require('packer').startup(function(use)
+
+    use 'github/copilot.vim'
+    use 'puremourning/vimspector'
+    use '907th/vim-auto-save'
+    use 'tpope/vim-fugitive'
+    use 'airblade/vim-gitgutter'
+    use 'vim-airline/vim-airline'
+    use 'knsh14/vim-github-link'
+    use 'tpope/vim-commentary'
+    use 'tpope/vim-obsession'
+    use { 'junegunn/fzf', run = './install --bin', }
+    use 'junegunn/fzf.vim'
+
+end)
+
+
