2ef2f612037125f0928a46d1f6c9a544e310827d
pierre <rischpierre@gmail.com>
2022-09-28 21:50:47 -0400

    install plugins via vim

--- init.lua
+++ init.lua
@@ -19,15 +19,14 @@ vim.api.nvim_set_keymap("n", "<A-j>", "<C-w>j", {noremap = true, silent = true})
 vim.api.nvim_set_keymap("n", "<A-k>", "<C-w>k", {noremap = true, silent = true})
 vim.api.nvim_set_keymap("n", "<A-h>", "<C-w>h", {noremap = true, silent = true})
 vim.api.nvim_set_keymap("n", "<A-l>", "<C-w>l", {noremap = true, silent = true})
+
+-- TODO keymap for resizing the window
 vim.api.nvim_set_keymap("n", "<A-S-\\=>", "<C-w>+", {noremap = true, silent = true})
 -- vim.api.nvim_set_keymap("n", "<C-A-S-\\=>", "<C-w>-", {noremap = true, silent = true})
 
 vim.api.nvim_set_keymap("n", "<A-c>", '":+y<CR>', {noremap = true, silent = true})
 vim.api.nvim_set_keymap("t", "<Esc>", "<C-\\><C-n>", {noremap = true, silent = true}) -- in order to use ESC to exit terminal mode
-vim.api.nvim_set_keymap("n", "<Esc>", ":noh<CR>", {noremap = true, silent = true})
-
--- set word uppercase
-vim.api.nvim_set_keymap("n", "<A-S-U>", "viw<S-U>e", {noremap = true, silent = true})
+vim.api.nvim_set_keymap("n", "<Esc>", ":noh<CR>", {noremap = true, silent = true}) -- set word uppercase vim.api.nvim_set_keymap("n", "<A-S-U>", "viw<S-U>e", {noremap = true, silent = true})
 
 
 vim.g.auto_save = true
@@ -35,46 +34,31 @@ vim.g.auto_save = true
 -- Fzf config
 vim.api.nvim_set_keymap("n", "<C-p>", ":Files<CR>", {noremap = true, silent = false})
 vim.api.nvim_set_keymap("n", "<C-o>", ":Buffers<CR>", {noremap = true, silent = false})
-vim.g.fzf_layout = {window = {down = '40%'}}
+vim.g.fzf_layout = {down = '40%'}
 
 -- vimspector config
--- TODO
--- nnoremap <leader>sd :call vimspector#Launch()<CR>
--- nnoremap <leader>kd :call vimspector#Reset()<CR>
--- nnoremap <leader>c :call vimspector#Continue()<CR>
--- nnoremap <leader>n :call vimspector#StepOver()<CR>
--- nnoremap <leader>so :call vimspector#StepOut()<CR>
--- nnoremap <leader>si :call vimspector#StepInto()<CR>
--- nnoremap <leader>r :call vimspector#RunToCursor()<CR>
--- nnoremap <leader>bp :call vimspector#ToggleBreakpoint()<CR>
+vim.api.nvim_set_keymap("n", "<leader>sd", ':call vimspector#Launch()<CR>', {noremap = true, silent = false})
+vim.api.nvim_set_keymap("n", "<leader>kd", ':call vimspector#Reset()<CR>', {noremap = true, silent = false})
+vim.api.nvim_set_keymap("n", "<leader>c", ':call vimspector#Continue()<CR>', {noremap = true, silent = false})
+vim.api.nvim_set_keymap("n", "<leader>n", ':call vimspector#StepOver()<CR>', {noremap = true, silent = false})
+vim.api.nvim_set_keymap("n", "<leader>so", ':call vimspector#StepOut()<CR>', {noremap = true, silent = false})
+vim.api.nvim_set_keymap("n", "<leader>si", ':call vimspector#StepInto()<CR>', {noremap = true, silent = false})
+vim.api.nvim_set_keymap("n", "<leader>r", ':call vimspector#RunToCursor()<CR>', {noremap = true, silent = false})
+vim.api.nvim_set_keymap("n", "<leader>bp", ':call vimspector#ToggleBreakpoint()<CR>', {noremap = true, silent = false})
 
--- bootstrap packer
-local ensure_packer = function()
-  local fn = vim.fn
-  local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
-  if fn.empty(fn.glob(install_path)) > 0 then
-    fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
-    vim.cmd [[packadd packer.nvim]]
-    return true
-  end
-  return false
-end
-local packer_bootstrap = ensure_packer()
-
-return require('packer').startup(function(use)
-
-    use 'github/copilot.vim'
-    use 'puremourning/vimspector'
-    use '907th/vim-auto-save'
-    use 'tpope/vim-fugitive'
-    use 'airblade/vim-gitgutter'
-    use 'vim-airline/vim-airline'
-    use 'knsh14/vim-github-link'
-    use 'tpope/vim-commentary'
-    use 'tpope/vim-obsession'
-    use { 'junegunn/fzf', run = './install --bin', }
-    use 'junegunn/fzf.vim'
-
-end)
-
-
+vim.cmd [[
+call plug#begin()
+Plug 'github/copilot.vim'
+Plug 'puremourning/vimspector'
+Plug '907th/vim-auto-save'
+Plug 'NLKNguyen/papercolor-theme'
+Plug 'tpope/vim-fugitive'
+Plug 'airblade/vim-gitgutter'
+Plug 'vim-airline/vim-airline'
+Plug 'junegunn/fzf', { 'do': { -> fzf#install() } }
+Plug 'junegunn/fzf.vim'
+Plug 'knsh14/vim-github-link'
+Plug 'tpope/vim-commentary'
+Plug 'tpope/vim-obsession'
+call plug#end()
+]]
