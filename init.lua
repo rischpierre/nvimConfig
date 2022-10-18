fb80951bd4331e5f985465089205aa7d6a939898
prisch <rischpierre@gmail.com>
2022-10-18 08:04:59 -0400

    add indent lines plugin

--- init.lua
+++ init.lua
@@ -102,6 +102,7 @@ Plug 'nvim-treesitter/nvim-treesitter', {'do': ':TSUpdate'}
 Plug 'neovim/nvim-lspconfig'
 Plug 'williamboman/nvim-lsp-installer'
 Plug 'gcmt/taboo.vim'
+Plug 'lukas-reineke/indent-blankline.nvim'
 call plug#end()
 
 colorscheme PaperColor
@@ -113,6 +114,28 @@ let g:airline#extensions#tabline#fnamemod = ':t'
 vim.cmd [[
     syntax keyword Todo todo   
 ]]
+
+
+-- line indent
+-- vim.opt.termguicolors = true
+vim.cmd [[highlight IndentBlanklineIndent1 guifg=#E06C75 gui=nocombine]]
+vim.cmd [[highlight IndentBlanklineIndent2 guifg=#E5C07B gui=nocombine]]
+vim.cmd [[highlight IndentBlanklineIndent3 guifg=#98C379 gui=nocombine]]
+vim.cmd [[highlight IndentBlanklineIndent4 guifg=#56B6C2 gui=nocombine]]
+vim.cmd [[highlight IndentBlanklineIndent5 guifg=#61AFEF gui=nocombine]]
+vim.cmd [[highlight IndentBlanklineIndent6 guifg=#C678DD gui=nocombine]]
+
+require("indent_blankline").setup {
+    space_char_blankline = " ",
+    char_highlight_list = {
+        "IndentBlanklineIndent1",
+        "IndentBlanklineIndent2",
+        "IndentBlanklineIndent3",
+        "IndentBlanklineIndent4",
+        "IndentBlanklineIndent5",
+        "IndentBlanklineIndent6",
+    },
+}
 
 -- treesitter config
 require'nvim-treesitter.configs'.setup {
